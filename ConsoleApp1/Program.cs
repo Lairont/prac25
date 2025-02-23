41a45d9d1cef727d09254815ff5635420970cdd4
Никита <153456515+Lairont@users.noreply.github.com>
2025-02-24 01:13:15 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Software soft1 = new Software("Windows", "Microsoft", false);
+            Console.WriteLine($"{soft1.ToString()}");
+            soft1.Check();
+            Console.WriteLine();
+            soft1.Change();
+            Console.WriteLine($"{soft1.ToString()}");
+            soft1.Check();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Software.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Software
+    {
+        private string name;
+        private string manufacturer;
+        private bool poidfree;
+
+        public string Name {get {return (name != "") ? name : "Неизвестно"; } set => name = value; }
+            
+        public string Manufacturer {get {return (manufacturer != "") ? manufacturer : "Неизвестно"; } set => manufacturer = value; }
+        public bool Poidfree { get => poidfree; set => poidfree = value; }
+
+        public Software() {}
+        public Software(string name, string  manufacturer, bool poidfree)
+        {
+            this.Name = name;
+            this.Manufacturer = manufacturer;
+            this.Poidfree = poidfree;
+        }
+        public override string ToString()
+        {
+            return $"Название:{Name}\nПроизводитель:{Manufacturer}\n{(Poidfree ? "Бесплатное" : "Платное")}";
+        }
+        public void Change()
+        {
+            Poidfree = !Poidfree;
+        }
+        public void Check()
+        {
+            if(!Poidfree)
+            {
+                Console.WriteLine("ПО платное");
+            }
+            else
+            {
+                Console.WriteLine("ПО бесплатное");
+            }
+        }
+    }
+}
